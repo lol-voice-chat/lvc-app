1cd382e66ef32ff9c5e68738b1523294a8fb6c0a
nswon <swnam0729@gmail.com>
2023-09-20 20:00:44 +0900

    fix

--- src/main/league/league-title.ts
+++ src/main/league/league-title.ts
@@ -16,11 +16,17 @@ interface SummonerMatchHistory {
 
 export const pickLeagueTitle = (team: any[]) => {
   ipcMain.on('league-title', async (event, leagueTitleList: LeagueTitle[]) => {
-    if (team.length <= 1) {
+    /**
+     * 만약 팀의 소환사가 1명밖에 없다면 null 반환
+     */
+    if (team.length === 1) {
       event.reply('league-title', null);
       return;
     }
 
+    /**
+     * 팀 소환사 전적 리스트
+     */
     const summonerMatchHistoryList: SummonerMatchHistory[] = [];
 
     for (const summoner of team) {
@@ -37,20 +43,27 @@ export const pickLeagueTitle = (team: any[]) => {
     }
 
     const result: any[] = [];
-    let summonerCount = 0;
-    while (summonerCount !== summonerMatchHistoryList.length - 1) {
-      const leagueTitle = leagueTitleList[summonerCount];
-      summonerCount++;
-      const array: any[] = [];
+    let leagueTitleIndex = 0;
 
+    /**
+     * 우리팀 소환사 수만큼 돌린다.
+     */
+    for (let i = 0; i < summonerMatchHistoryList.length; i++) {
+      /**
+       * 칭호데이터를 순서대로 들고온다.
+       */
+      const leagueTitle = leagueTitleList[leagueTitleIndex];
+      console.log('이번 칭호: ', leagueTitle.title);
+      leagueTitleIndex += 1;
+
+      const array: any[] = [];
       summonerMatchHistoryList.forEach((summonerMatchHistory: SummonerMatchHistory) => {
         let count = 0;
 
-        //각 소환사 100판 전적 돌면서
+        //1. 소환사 pvp match를 돌면서 칭호 데이터값을 더한다.
         summonerMatchHistory.pvpMatchList.forEach((match: MatchData) => {
           const participant: ParticipantData = match.participants[0];
 
-          //칭호 데이터를 더한다
           let statsValue = participant.stats[leagueTitle.value as keyof ParticipantData['stats']];
           if (statsValue) {
             statsValue = 1;
@@ -61,37 +74,35 @@ export const pickLeagueTitle = (team: any[]) => {
           count += statsValue;
         });
 
-        //summonerId
-        const test = {
+        const statsValue = {
           summonerId: summonerMatchHistory.summonerId,
           count,
         };
 
-        array.push(test);
+        array.push(statsValue);
       });
 
       if (leagueTitle.standard === 'max') {
         const temp = array.sort((a, b) => a.count - b.count);
-        const goodSummoner = temp[temp.length - 1];
-        console.log('good: ', goodSummoner);
+        const summoner = temp[temp.length - 1];
 
         result.push({
-          summonerId: goodSummoner.summonerId,
+          summonerId: summoner.summonerId,
           title: leagueTitle.title,
           description: leagueTitle.description,
         });
       } else {
         const temp = array.sort((a, b) => b.count - a.count);
-        const goodSummoner = temp[temp.length - 1];
-        console.log('good: ', goodSummoner);
+        const summoner = temp[temp.length - 1];
 
         result.push({
-          summonerId: goodSummoner.summonerId,
+          summonerId: summoner.summonerId,
           title: leagueTitle.title,
           description: leagueTitle.description,
         });
       }
     }
+    console.log('최종 칭호 매칭: ', result);
 
     event.reply('league-title', result);
     return;
