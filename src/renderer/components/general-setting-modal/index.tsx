651793c25fb2c4376f4bfb2c74e738b98f1e95e9
tkyung05 <tkyung6761@gmail.com>
2023-10-09 23:38:06 +0900

    tk

--- src/renderer/components/general-setting-modal/index.tsx
+++ src/renderer/components/general-setting-modal/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import * as _ from './style';
 import electronStore from '../../@store/electron';
 import { GeneralSettingsConfigType } from '../../@store/atom';
+import Inko from 'inko';
 
 function GeneralSettingModal(props: {
   handleClickModalTrigger: () => void;
@@ -14,6 +15,7 @@ function GeneralSettingModal(props: {
   const [muteMicShortcutKey, setMuteMicShortcutKey] = useState(
     props.settingsConfig.muteMicShortcutKey
   );
+  const [isGettingShortcutKey, setIsGettingShortcutKey] = useState(false);
 
   const handleClickSaveButton = () => {
     const config = {
@@ -23,6 +25,25 @@ function GeneralSettingModal(props: {
     };
     electronStore.set('general-settings-config', config);
     props.handleClickModalTrigger();
+  };
+
+  const handleKeyDown = (e: any) => {
+    if (!isGettingShortcutKey) return;
+    e.preventDefault();
+
+    let { is한글, ko2en } = new Inko();
+    let value = e.key;
+
+    if (is한글(value)) {
+      value = ko2en(value);
+    }
+    isPressToTalk ? setPressToTalkShortcutKey(value) : setMuteMicShortcutKey(value);
+    setIsGettingShortcutKey(false);
+  };
+
+  const handleClickGettingShortcutKey = () => {
+    setIsGettingShortcutKey((prev) => !prev);
+    document.getElementById('input-shortcut-key')?.focus();
   };
 
   return (
@@ -36,7 +57,11 @@ function GeneralSettingModal(props: {
       </div>
 
       {isPressToTalk ? (
-        <div>눌러서 말하기 단축키 설정 - {pressToTalkShortcutKey}</div>
+        <>
+          <div>눌러서 말하기 단축키 설정</div>
+          <div onClick={handleClickGettingShortcutKey}>{pressToTalkShortcutKey}</div>
+          <input id="input-shortcut-key" onKeyDown={handleKeyDown} />
+        </>
       ) : (
         <div>마이크 음소거 단축키 설정 - {muteMicShortcutKey}</div>
       )}
