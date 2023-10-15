72fd90ab489f4f388fb5d373b67f1ac83031f0ac
tkyung05 <tkyung6761@gmail.com>
2023-10-15 15:04:10 +0900

    tk

--- src/renderer/components/@layout/nav-bar/index.tsx
+++ src/renderer/components/@layout/nav-bar/index.tsx
@@ -1,58 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { SummonerType } from '../../../@type/summoner';
-import { useRecoilState, useSetRecoilState } from 'recoil';
-import { summonerState, gameStatusState, leagueChampInfoListState } from '../../../@store/atom';
-import { IPC_KEY } from '../../../../const';
-import electronStore from '../../../@store/electron';
+import React, { useState } from 'react';
 import VoiceRoomModal from '../../voice-room-modal';
 import * as S from './style';
 import { useNavigate } from 'react-router-dom';
 import { PATH } from '../../../const';
-
-const { ipcRenderer } = window.require('electron');
+import useLeagueHandler from '../../../hooks/use-league-handler';
 
 function NavBar() {
-  const [gameStatus, setGameStatus] = useRecoilState(gameStatusState);
-  const setSummoner = useSetRecoilState(summonerState);
-  const setLeagueChampInfoList = useSetRecoilState(leagueChampInfoListState);
+  const { gameStatus } = useLeagueHandler();
 
   const [onClickTag, setOnClickTag] = useState(PATH.HOME);
-
   const navigate = useNavigate();
-
-  useEffect(() => {
-    /* 롤 클라이언트 on */
-    ipcRenderer.on(IPC_KEY.ON_LEAGUE_CLIENT, (_, summoner: SummonerType) => {
-      setSummoner(summoner);
-    });
-
-    /* 롤 클라이언트 off */
-    ipcRenderer.on(IPC_KEY.SHUTDOWN_APP, () => {
-      setGameStatus('none');
-    });
-
-    /* 챔피언 선택창 on */
-    ipcRenderer.on(IPC_KEY.TEAM_JOIN_ROOM, (_, { roomName }) => {
-      setGameStatus('champ-select');
-      electronStore.set('team-voice-room-name', roomName);
-    });
-
-    /* 인게임 전 로딩창 on */
-    ipcRenderer.on(IPC_KEY.LEAGUE_JOIN_ROOM, (_, { roomName, teamName, summonerDataList }) => {
-      if (teamName === roomName) return;
-
-      setGameStatus('loading');
-      setLeagueChampInfoList(summonerDataList);
-      electronStore.set('league-voice-room-name', { roomName, teamName });
-    });
-
-    return () => {
-      ipcRenderer.removeAllListeners(IPC_KEY.ON_LEAGUE_CLIENT);
-      ipcRenderer.removeAllListeners(IPC_KEY.SHUTDOWN_APP);
-      ipcRenderer.removeAllListeners(IPC_KEY.TEAM_JOIN_ROOM);
-      ipcRenderer.removeAllListeners(IPC_KEY.LEAGUE_JOIN_ROOM);
-    };
-  }, []);
 
   const handleClickCategoryTag = (path: string) => {
     navigate(path);
