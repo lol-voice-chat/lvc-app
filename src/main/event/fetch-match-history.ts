e380454f1b1709946447568c35afc3aa80a8a976
nswon <swnam0729@gmail.com>
2023-10-19 00:03:16 +0900

    fix

--- src/main/event/fetch-match-history.ts
+++ src/main/event/fetch-match-history.ts
@@ -4,10 +4,28 @@ import { IPC_KEY } from '../../const';
 import { Friends } from '../models/friends';
 import { redisClient } from '../lib/redis-client';
 import { credentials } from '../lvc-application';
+import { createHttp1Request } from 'league-connect';
 
 export const handleFetchMatchHistoryEvent = () => {
   ipcMain.handle(IPC_KEY.FETCH_MATCH_HISTORY, async (event, { puuid, isMine }) => {
     if (!credentials) {
+      return {
+        summonerStats: null,
+        isFriend: null,
+        isError: true,
+      };
+    }
+
+    try {
+      //롤 프로그램이 완전히 꺼지기 전 체크
+      await createHttp1Request(
+        {
+          method: 'GET',
+          url: '/lol-chat/v1/me',
+        },
+        credentials!
+      );
+    } catch (error) {
       return {
         summonerStats: null,
         isFriend: null,
